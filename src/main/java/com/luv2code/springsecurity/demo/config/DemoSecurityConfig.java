99a53fa76a046bd3c65f6356371d17db7498ba27
OliverZott <zott_oliver@web.de>
2021-01-13 20:20:47 +0100

    Implementing first in-memory user/pw/role example

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +12,14 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+
+		User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+		auth.inMemoryAuthentication()
+				.withUser(users.username("user1").password("pw1").roles("EMPLOYEE"))
+				.withUser(users.username("user2").password("pw2").roles("EMPLOYEE"))
+				.withUser(users.username("user3").password("pw3").roles("EMPLOYEE"));
+
 	}
 
 	
