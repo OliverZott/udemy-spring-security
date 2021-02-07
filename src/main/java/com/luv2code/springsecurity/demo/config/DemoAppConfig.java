e539620a67702a86bbe3bafcdf73e667f863b975
OliverZott <zott_oliver@web.de>
2021-02-07 13:50:38 +0100

    Adding properties file for Environment, configure data source

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,24 +1,77 @@
 package com.luv2code.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+/**
+ * add PropertySource: src/main/resources files automatically copied to classpath during maven build
+ */
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig implements WebMvcConfigurer {
+
+	// helper class will hold data from property-file
+	private final Environment env;
+	// logger for convenience
+	private final Logger logger = Logger.getLogger(getClass().getName());
+
+	public DemoAppConfig(Environment env) {
+		this.env = env;
+	}
+
+	//define bean for security data source object
+	@Bean
+	public DataSource securityDataSource() {
+
+		// create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+		// set the jdbc driver
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException();
+		}
+
+		// log connection props
+		logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+		// set database connection props
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+		// set connection pool properties
+		securityDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+		securityDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+		securityDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+		securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+
+		return null;
+	}
+
 
 	/**
 	 * Define bean for view-resolver, so spring-mvc knows where to look for the
 	 * views: e.g: /WEB-INF/view/show-some-webpage.jsp
-	 * 
-	 * @return
+	 *
+	 * @return viewResolver InternalResourceViewResolver is used to resolve the provided URI to actual URI.
 	 */
 	@Bean
 	public ViewResolver viewResolver() {
@@ -34,8 +87,8 @@ public class DemoAppConfig implements WebMvcConfigurer {
 	/**
 	 * Configure the all Java configuration to serve content from the "/css"
 	 * directory
-	 * 
-	 * @param registry
+	 *
+	 * @param registry Stores registrations of resource handlers for serving static resources (e.g. CSS).
 	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
